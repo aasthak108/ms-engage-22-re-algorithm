95d008890d219ca866c32a1acd3826901afdd712
Aastha Kumari <aasthak108@gmail.com>
2022-05-20 16:52:59 +0530

    added web functionality

---
+++ re-algorithm/src/main/java/com/msengage/realgorithm/RecommendationEngineApp.java
@@ -0,0 +1,14 @@
+package com.msengage.realgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RecommendationEngineApp
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(RecommendationEngineApp.class, args);
+	}
+}
