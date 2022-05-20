e161d1844f5cf33674e501befd7ddedbe146c095
Aastha Kumari <aasthak108@gmail.com>
2022-05-20 16:13:14 +0530

    initial commit

---
+++ re-algorithm/src/test/java/com/msengage/realgorithm/ReAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msengage.realgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
