b67809c1a388bf2d14ccb027aa6c23e927f9c2bc
Aastha Kumari <aasthak108@gmail.com>
2022-05-21 22:24:02 +0530

    update

--- re-algorithm/src/main/java/com/msengage/realgorithm/controller/RecommendationEngineController.java
+++ re-algorithm/src/main/java/com/msengage/realgorithm/controller/RecommendationEngineController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -35,7 +36,7 @@ public class RecommendationEngineController
         {
             File movieDataJSON = new ClassPathResource(
                     "/dataset/data-movie.json").getFile();
-            movieList = mapper.readValue(movieDataJSON, ArrayList.class);
+            movieList = Arrays.asList(mapper.readValue(movieDataJSON, Movie[].class));
             User user = getUserById(customerId);
             recommendedMovies = getMovieRecommendation(movieList, user);
         }
@@ -48,7 +49,7 @@ public class RecommendationEngineController
 
     private List<String> getMovieRecommendation(List<Movie> movieList, User user)
     {
-        List<String> recommendedMovies = null;
+        List<String> recommendedMovies = new ArrayList<>();
         List<String> featuresLiked = new ArrayList<>();
         for(Movie movie :  user.getWatchlist())
         {
@@ -71,12 +72,11 @@ public class RecommendationEngineController
     {
         User selectedUser = null;
         ObjectMapper mapper = new ObjectMapper();
-        File userData = null;
         List<User> userList = null;
         try
         {
-            userData = new ClassPathResource("/dataset/data-user.json").getFile();
-            userList = mapper.readValue(userData, ArrayList.class);
+            File userDataJSON = new ClassPathResource("/dataset/data-user.json").getFile();
+            userList = Arrays.asList(mapper.readValue(userDataJSON, User[].class));
             for(User user : userList)
             {
                 if(user.getUserId().equalsIgnoreCase(id))
